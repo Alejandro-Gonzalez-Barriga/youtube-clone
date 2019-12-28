8b88cc1f21999c5c9bf5c2c78dd70d54d755eb94
alexG <alejandro.gonzalez.barriga@gmail.com>
2019-12-27 18:56:08 -0600

    Updates

---
+++ precache-manifest.74762a1db76c33a3e13ef4af7e9851d4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2425d14b053efb4fcefb31cd12a4c71",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "b75d66111e4df2b0552b",
+    "url": "/youtube-clone/static/js/2.65ec8e0b.chunk.js"
+  },
+  {
+    "revision": "f4d2969ba3c7accdaa7a",
+    "url": "/youtube-clone/static/js/main.872392db.chunk.js"
+  },
+  {
+    "revision": "7c348438a34bf75c231e",
+    "url": "/youtube-clone/static/js/runtime~main.a1cbaaee.js"
+  }
+]);
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-//we will be using this index file to export all of the components to
-//the App.js file, this is done simply to maintain the project organized
-
-export { default as SearchBar } from './SearchBar';
-export { default as VideoDetail } from './VideoDetail';
-export { default as VideoList } from './VideoList';
